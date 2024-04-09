3823f7f78f121ea8e6f5f1a6a8a9ea358788563b
Teodora Ardeleanu <teodora.ardeleanu@eestec.ro>
2024-04-09 13:52:02 +0300

    A,B,C1 solved

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04SecondaryActivity.java
@@ -0,0 +1,27 @@
+package ro.pub.cs.systems.eim.practicaltest01var04;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class PracticalTest01Var04SecondaryActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
